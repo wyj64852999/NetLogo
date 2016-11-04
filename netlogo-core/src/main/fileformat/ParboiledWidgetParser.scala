e3fa8f74110db2c64d13a421a4489a07610ac224
Robert Grider <robert.grider@northwestern.edu>
2016-11-04 09:48:37 -0500

    Convert HubNet widget readers to parboiled2

--- netlogo-core/src/main/fileformat/ParboiledWidgetParser.scala
+++ netlogo-core/src/main/fileformat/ParboiledWidgetParser.scala
@@ -92,32 +92,3 @@ trait DefaultParboiledWidgetParser extends ParboiledWidgetParser {
     ReportingParseRunner(rule).run(formattedLines).result
   }
 }
-
-trait ParsingStringUtils {
-  def stripLines(st: String): String =
-    st.flatMap{
-      case '\n' => "\\n"
-      case '\\' => "\\\\"
-      case '\"' => "\\\""
-      case c => c.toString
-    }
-
-  def restoreLines(s: String): String = {
-    @scala.annotation.tailrec
-    def loop(acc: Vector[Char], rest: String): Vector[Char] = {
-      if (rest.size < 2)
-        acc ++ rest
-      else if (rest.head == '\\')
-        rest.tail.head match {
-          case 'n'  => loop(acc :+ '\n', rest.tail.tail)
-          case '\\' => loop(acc :+ '\\', rest.tail.tail)
-          case '"'  => loop(acc :+ '"', rest.tail.tail)
-      case _    => sys.error("invalid escape sequence in \"" + s + "\"")
-        }
-        else loop(acc :+ rest.head, rest.tail)
-    }
-    loop(Vector(), s).mkString
-  }
-}
-
-object ParsingStringUtils extends ParsingStringUtils
